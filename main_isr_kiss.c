a10cbb31d6f7cd09913d144596e8406300dcbbd0
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-02-14 18:28:22 -0300

    Mais correções

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -8,8 +8,7 @@ int batata;
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
-  printf("btn pressed \n");
-
+  
   if (g_cnt >= 8)
     g_cnt = 0;
 
