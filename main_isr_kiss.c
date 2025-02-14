aa612b8952f8c83d78d16850455f29c05a074dd4
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-02-14 18:39:49 -0300

    Mudanças para regras 3.2. e 3.4.

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -2,7 +2,7 @@
 
 // COmentário só para ver o erro do log
 volatile int g_cnt = 0;
-
+volatile bool update_display = false;
 
 
 // This code creates a progress bar on an OLED screen that
@@ -10,26 +10,32 @@ volatile int g_cnt = 0;
 
 void btn_callback(void) {
   
-  char g_str[10];
   if (g_cnt >= 8)
     g_cnt = 0;
 
   g_cnt = 2;
   
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  update_display = true;
+
 }
 
+void update_display(void){
+  if (update_display){
+    char g_str[10];
+    int i;
+    for (i = 0 ; i < g_cnt ; i++){
+      g_str[i] = '\0';
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+      update_display = false; 
+    }
+  }
+}
 void main(void) {
   // ...
 
   int batata;
   batata = batata + 1;
   while (1) {
+    update_display()
   }
 }
